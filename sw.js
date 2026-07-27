62ab23c3aea119fb051c3719216c78f9dcd270b7
David Brian Ingalls <triggger0357@gmail.com>
2026-07-27 01:12:37 -0700

    Add offline PWA service worker and manifest

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'sovereign-hub-v1';
+
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './styles/main.css',
+  './scripts/app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.startsWith('ws://') || event.request.url.startsWith('wss://')) {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
